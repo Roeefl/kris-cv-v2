42f8a93d7d9177c1740ba7daf1daf34e99123972
Roee Fl <roee.fleishman@meetsnappy.com>
2021-05-21 18:01:03 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import MainPage from './views/MainPage';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      MainPage,
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-family: 'Heebo', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: gray;
+    color: #2c3e50;
+    direction: rtl;
+    display: flex;
+    justify-content: center;
+    padding: 16px;
+  }
 </style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <aside class="side">
+      <h3 class="personal-title">
+        פרטים אישיים
+      </h3>
+      <p v-for="(value, key) in personalFields" :key="key" :class="key">
+        {{ value }}
+      </p>
+    </aside>
+    <main class="main">
+      <h1 class="name">
+        {{ name }}
+      </h1>
+      <h2 class="role">
+        {{ role }}
+      </h2>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String,
+    },
+    data: () => ({
+      name: 'קריסטינה קרפה קורבוט אוריה',
+      personalFields: {
+        role: 'מטפלת בשיטת אלבאום',
+        address: 'חיפה, ישראל',
+        phone: '0546-398-195',
+        email: '25kristy25@gmail.com',
+        birthDate: '25.07.1989',
+      },
+    }),
+  }
+</script>
+
+<style scoped lang="scss">
+  ul {
+    list-style-type: none;
+    padding: 0;
+
+      li {
+      display: inline-block;
+      margin: 0 10px;
+    }
+  }
+
+  .container {
+    width: 1000px;
+    display: grid;
+    grid-template-columns: 30% 70%;
+
+    .side {
+      padding: 24px 8px;
+      background: #6b385c;
+      color: white;
+    }
+
+    .main {
+      padding: 24px 8px;
+      background: white;
+      color: black;
+    }
+  }
+</style>
